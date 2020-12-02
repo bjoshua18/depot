e4fb8c913c4260b0d0b3dde5b79162870fe2e6f3
Byron Bustamante <byronbl.12@gmail.com>
2020-12-02 11:20:49 +0000

    Creamos los modelos Cart y LineItem y la relación entre ellos y Product. Implementamos la funcionalidad de agregar un product al cart y la navegación del comprador

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,11 @@
 class StoreController < ApplicationController
   def index
+    count_visit
     # SELECT products.* FROM products ORDER BY product.title ASC
     @products = Product.order(:title)
+  end
+
+  def count_visit
+    session[:counter] = session[:counter].nil? ? 1 : session[:counter] + 1
   end
 end
