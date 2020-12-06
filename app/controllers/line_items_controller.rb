0db88b1146120baa58decc52f4c9dfb3b9e883ea
Byron Bustamante <byronbl.12@gmail.com>
2020-12-06 12:49:04 +0000

    Task F

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create, :destroy]
+  before_action :set_cart, only: [:create, :update, :destroy]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -46,12 +46,18 @@ class LineItemsController < ApplicationController
   # PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      if (params[:line_item][:quantity].to_i > 0) # Si cuantity > 0
+        if @line_item.update(line_item_params)
+          format.html { redirect_to @cart, notice: 'Line item was successfully updated.' }
+          format.js { @current_item = @line_item }
+          format.json { render :show, status: :ok, location: @line_item }
+        else
+          format.html { render :edit }
+          format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        end
+      else # Si quantity <= 0
+        @line_item.destroy
+        format.js
       end
     end
   end
@@ -75,6 +81,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id)
+      params.require(:line_item).permit(:product_id, :quantity)
     end
 end
