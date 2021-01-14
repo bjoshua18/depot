7378e561c6bd59a143496525eb8e3504187efe71
Byron Bustamante <byronbl.12@gmail.com>
2021-01-14 19:23:48 +0000

    Task L

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 require 'pago'
 
 class Order < ApplicationRecord
+	belongs_to :pay_type
 	has_many :line_items, dependent: :destroy
 
 	validates :name, :address, :email, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  get 'support_requests/index'
+  get 'support_requests/update'
   get 'admin' => 'admin#index'
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
+
   resources :users
   resources :pay_types
   resources :products do
     get :who_bought, on: :member
   end
+
+  resources :support_requests, only: [:index, :update]
 
   scope '(:locale)' do
     resources :orders
